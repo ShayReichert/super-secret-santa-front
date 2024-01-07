3aaea1aa11ecfe755f1c9f0c8a185f1c44a5995b
Shay Reichert <shay.reichert@outlook.fr>
2024-01-07 18:17:40 +0100

    Not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,19 @@
+import SantaImage from "./components/SantaImage/SantaImage";
 import styles from "./styles/not-found.module.scss";
+import { Titan_One } from "next/font/google";
+
+const titan_one = Titan_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function NotFound() {
   return (
     <main id="main" className={styles["not-found"]}>
-      <section>
-        <div>
-          {/* Image wrapper */}
-          {/* IMAGE */}
-          <h1>404</h1>
-          <p>Oups, cette page n'existe pas !</p>
-        </div>
-      </section>
+      {/* Image wrapper */}
+      {/* IMAGE */}
+      <div className={styles["content"]}>
+        <h1 className={titan_one.className}>404</h1>
+        <p>Oups, cette page n'existe pas !</p>
+      </div>
+      <SantaImage />
     </main>
   );
 }
